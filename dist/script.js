c2ca7b28f2154abf1fcfa5cd260c9565cf15a932
AbdulrhmanMothanakhalil <96795805+AbdulrhmanMothanakhalil@users.noreply.github.com>
2023-10-12 20:13:01 +0300

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+const mobilebtn= document.querySelector("#mobilebutton");
+const mobilemenu= document.querySelector("#mobile-menu");
+
+mobilebtn.addEventListener('click', ()=>{
+
+mobilemenu.classList.toggle('hidden');
+
+});
